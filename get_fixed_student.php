0fd56e698585c4518ea39a39b0f196beb9c7921e
zhaofeng-shu33 <616545598@qq.com>
2019-03-25 06:40:03 +0800

    not allow query flow student in fixed student request

--- get_fixed_student.php
+++ get_fixed_student.php
@@ -5,6 +5,9 @@ $group = $_GET['student_group'];
 if($group == null || $group == ''){
     exitJson(1, 'null group');
 }
+if($group == '流动')
+    exitJson(0, 'result for flow is not allowed', array('student_list'=>[]));
+
 $db = getDb();
 $sql = 'select s.name, s.school from '.getTablePrefix().'_student as s, '.getTablePrefix()."_group as g where g.name = '$group' and g.id = s.group_id";
 $res=mysqli_query($db, $sql) or die(mysqli_error($db));
