93220a0da0ad85424b2a75dd52a76bc1dbb819c2
zhaofeng-shu33 <616545598@qq.com>
2019-03-25 02:12:36 +0800

    another fun name fix

--- add_activity.php
+++ add_activity.php
@@ -29,7 +29,7 @@ else{
 // 2019-3-4 ~ week 1
 $date=date_create("2019-3-4");
 $interval_int = 7 * ($week - 1);
-$date_add($date, date_interval_create_from_date_string($interval_int." days"));
+date_add($date, date_interval_create_from_date_string($interval_int." days"));
 $sql = 'insert into '.getTablePrefix()."_activity (name, location, time) values ('$name', '$location', $date)";
 $res=mysqli_query($db, $sql) or die(mysqli_error($db));
 $activity_id = mysqli_insert_id($db);
