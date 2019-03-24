34b99a321a5e77ca140dcf9e0e0a35cbe30f88c6
zhaofeng-shu33 <616545598@qq.com>
2019-03-25 01:01:01 +0800

    remove $

--- get_student_list.php
+++ get_student_list.php
@@ -6,7 +6,7 @@ if($name_prefix == null || $name_prefix == ''){
     exitJson(1, 'null name prefix');
 }
 $db = getDb();
-$sql = 'select name, school from '.getTablePrefix()."_student where group_id = 1 and name like '$$name_prefix%'";
+$sql = 'select name, school from '.getTablePrefix()."_student where group_id = 1 and name like '$name_prefix%'";
 $res=mysqli_query($db, $sql) or die(mysqli_error($db));
 $row = mysqli_fetch_assoc($res);
 exitJson(0, '',array('student_list'=>$row));
