f758e15bfb0bf427d48b1b8342089c44e1263b7b
zhaofeng-shu33 <616545598@qq.com>
2019-03-25 01:41:14 +0800

    use fetch all

--- get_student_list.php
+++ get_student_list.php
@@ -8,7 +8,7 @@ if($name_prefix == null || $name_prefix == ''){
 $db = getDb();
 $sql = 'select name, school from '.getTablePrefix()."_student where group_id = 1 and name like '$name_prefix%'";
 $res=mysqli_query($db, $sql) or die(mysqli_error($db));
-$row = mysqli_fetch_assoc($res);
+$row = mysqli_fetch_all($res);
 exitJson(0, '',array('student_list'=>$row));
 
 ?>
