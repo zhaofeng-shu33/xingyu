b4e4d4ed42bf31a8babf5a5e5057395b80532bee
zhaofeng-shu33 <616545598@qq.com>
2019-03-30 01:27:33 +0800

    two api modified

--- add_student_flow.php
+++ add_student_flow.php
@@ -17,11 +17,22 @@ $db = getDb();
 $sql_s = 'select id from '.getTablePrefix()."_student where name = '$name'";
 $res=mysqli_query($db, $sql_s) or die(mysqli_error($db));
 $row = mysqli_fetch_assoc($res);
-if($row['id'] != null){
-    exitJson(3, 'student already exists');
+if($row['id'] == null){ // student not exist
+    $sql = 'insert into '.getTablePrefix()."_student (name, school) values ('$name', '$school')";
+    $res=mysqli_query($db, $sql) or die(mysqli_error($db));    
+    $student_id =  mysqli_insert_id($db);
 }
-$sql = 'insert into '.getTablePrefix()."_student (name, school) values ('$name', '$school')";
-$res=mysqli_query($db, $sql) or die(mysqli_error($db));
-$student_id = mysqli_insert_id($db);
+else{// if student exists, don't trigger any error
+    $student_id = $row['id'];
+    $sql_sg = 'select id from '.getTablePrefix()."_semester_group where student_id = $student_id";
+    $res_sg=mysqli_query($db, $sql_sg) or die(mysqli_error($db));    
+    $row_sg = mysqli_fetch_assoc($res_sg);
+    if($row_sg['id'] != null){
+        exitJson(3, 'student already exists in current semester flow group');
+    }
+}
+$sql_ig = 'insert into '.getTablePrefix()."_semester_group (group_id, student_id) values (1, $student_id)";
+$res_ig=mysqli_query($db, $sql_ig) or die(mysqli_error($db));    
+
 exitJson(0, '',array('student_id'=>$student_id));
 ?>
--- get_student_list.php
+++ get_student_list.php
@@ -2,11 +2,21 @@
 include_once 'mysql.php';
 include_once 'functions.php';
 $name_prefix = $_GET['student_name_prefix'];
+$semester_id = $_GET['semester'];
 if($name_prefix == null || $name_prefix == ''){
     exitJson(1, 'null name prefix');
 }
+if($semester_id == null){
+    $semester_id = 2;
+}
+elseif(intval($semester_id) == 0){
+    exitJson(2, 'invalid semester');
+}
+else{
+    $semester_id = intval($semester_id);
+}
 $db = getDb();
-$sql = 'select name, school from '.getTablePrefix()."_student where group_id = 1 and name like '$name_prefix%'";
+$sql = 'select s.name, s.school from '.getTablePrefix().'_student as s, '.getTablePreifx()."_semester_group as sg where sg.group_id = 1 and sg.student_id = s.id and s.name like '$name_prefix%'";
 $res=mysqli_query($db, $sql) or die(mysqli_error($db));
 $row = mysqli_fetch_all($res);
 exitJson(0, '',array('student_list'=>$row));
