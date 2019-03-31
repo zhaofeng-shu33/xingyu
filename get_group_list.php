d75c8608cbb548b62f3a0aadb2b150d8374e543a
zhaofeng-shu33 <616545598@qq.com>
2019-03-31 23:36:52 +0800

    fix typo

--- get_group_list.php
+++ get_group_list.php
@@ -12,7 +12,7 @@ if(intval($semester_id) == 0){
 else{
     $semester_id = intval($semester_id);
 }
-$sql = 'select name from '.getTablePrefix()."_group where semester_id = $semeser_id";
+$sql = 'select name from '.getTablePrefix()."_group where semester_id = $semester_id";
 $res=mysqli_query($db, $sql) or die(mysqli_error($db));
 $row = mysqli_fetch_all($res);
 exitJson(0, '',array('group_list'=>$row));
