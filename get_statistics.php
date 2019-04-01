f02a672804aa6f698b5b1eeb2befd3916cc615c4
zhaofeng-shu33 <616545598@qq.com>
2019-04-02 00:21:10 +0800

    total student

--- get_statistics.php
+++ get_statistics.php
@@ -8,7 +8,11 @@ foreach($data as &$info){
     $sql_s = 'select count(s.id) as c from '.getTablePrefix().'_student as s, '.getTablePrefix()."_student_activity as sa where s.id = sa.student_id and s.school = '$school'";
     $res_s=mysqli_query($db, $sql_s) or die(mysqli_error($db));
     $row = mysqli_fetch_assoc($res_s);
+    $info['total_count'] = $row['c'];
+    $sql = 'select count(distinct s.id) as c from '.getTablePrefix().'_student as s, '.getTablePrefix()."_student_activity as sa where s.id = sa.student_id and s.school = '$school'";
+    $res=mysqli_query($db, $sql) or die(mysqli_error($db));
+    $row = mysqli_fetch_assoc($res);
     $info['total_student'] = $row['c'];
 }
-exitJson(0, $data);
+exitJson(0, '', $data);
 ?>
