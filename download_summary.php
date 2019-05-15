7c1781a876427784f1b9b304e3a6861b6759160a
zhaofeng-shu33 <616545598@qq.com>
2019-05-15 15:09:58 +0800

    + -> .

--- download_summary.php
+++ download_summary.php
@@ -7,14 +7,14 @@ if($school == null || array_search($school, $valid_school_list) === false){
 	exit();
 }
 $date_str = date('Y-m-d-');
-header('Content-Disposition: attachment; filename="' + $date_str + $school +'.csv"');
+header('Content-Disposition: attachment; filename="' . $date_str . $school .'.csv"');
 echo("姓名\t活动名称\t活动时间\t活动地点\n");
-$sql = "SELECT s.name, a.name, a.time, a.location from ".getTablePrefix()."_activity as a, ".getTablePrefix()."_student as s, ".getTablePrefix()."_student_activity as sa where sa.student_id = s.id and sa.activity_id = a.id and s.school = '" + $school + "'";
+$sql = "SELECT s.name, a.name, a.time, a.location from ".getTablePrefix()."_activity as a, ".getTablePrefix()."_student as s, ".getTablePrefix()."_student_activity as sa where sa.student_id = s.id and sa.activity_id = a.id and s.school = '" . $school . "'";
 $db = getDb();
 $res=mysqli_query($db, $sql) or die(mysqli_error($db));
 $row = mysqli_fetch_all($res);
 for($i=0; $i<count($row); $i++){
-	echo ($row[0] + '\t' + $row[1] + '\t' + $row[2] + '\t' + $row[3] + '\n');
+	echo ($row[0] . '\t' . $row[1] . '\t' . $row[2] . '\t' . $row[3] . '\n');
 }
 
 ?>
