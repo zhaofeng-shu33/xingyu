db41bbb1b4472b8caccf595fb08cab79162d507a
zhaofeng-shu33 <616545598@qq.com>
2019-05-15 14:59:05 +0800

    fix data_str

--- download_summary.php
+++ download_summary.php
@@ -6,8 +6,7 @@ $valid_school_list = array('thu', 'pku', 'hit', 'sust', 'szu');
 if($school == null || array_search($school, $valid_school_list) === false){
 	exit();
 }
-$date = date_default_timezone_get();
-$date_str = date_format($date, 'Y-m-d-');
+$date_str = date('Y-m-d-');
 header('Content-Disposition: attachment; filename="' + $date_str + $school +'.csv"');
 echo("姓名\t活动名称\t活动时间\t活动地点\n");
 $sql = "SELECT s.name, a.name, a.time, a.location from ".getTablePrefix()."_activity as a, ".getTablePrefix()."_student as s, ".getTablePrefix()."_student_activity as sa where sa.student_id = s.id and sa.activity_id = a.id and s.school = '" + $school + "'";
