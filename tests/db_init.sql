f92134a2509ada6110012c983aabf6d97db78600
zhaofeng-shu33 <616545598@qq.com>
2019-06-02 21:23:45 +0800

    add test get all group

--- tests/db_init.sql
+++ tests/db_init.sql
@@ -1,1 +1,12 @@
 insert into xingyu_student (name, school, wechat_openid) values ('赵丰', 'thu', 'abc');
+
+insert into xingyu_semester (name, start_time) values ('2018秋季学期', '2018-09-03');
+insert into xingyu_semester (name, start_time) values ('2019春季学期', '2019-03-04');
+
+insert into xingyu_group (name, semester_id) values ('流动', 2);
+insert into xingyu_group (name, semester_id) values ('周一下午', 2);
+insert into xingyu_group (name, semester_id) values ('周二下午', 2);
+insert into xingyu_group (name, semester_id) values ('周二下午', 1);
+
+
+
--- tests/mainTest.php
+++ tests/mainTest.php
@@ -18,4 +18,15 @@ class mainTest extends TestCase
         $json_out = json_decode($server_output);
         $this->assertEquals($json_out['err'], 0);
     }
+	public function test_get_all_group()
+	{
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $this->root . 'get_group_list.php');
+		curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
+        $server_output = curl_exec($ch);
+        $json_out = json_decode($server_output);
+        $this->assertEquals($json_out['err'], 0);	
+		$group_list = $json_out['group_list'];
+		$this->assertEquals(count($group_list), 4);
+	}
 }
