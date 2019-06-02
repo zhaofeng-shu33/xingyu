84a9e4a268a64cf1079c7efe8e4aeda97e2fd1e4
zhaofeng-shu33 <616545598@qq.com>
2019-06-02 21:49:51 +0800

    remove echo in test

--- tests/mainTest.php
+++ tests/mainTest.php
@@ -24,7 +24,6 @@ class mainTest extends TestCase
         curl_setopt($ch, CURLOPT_URL, $this->root . 'get_group_list.php?all=1');
 		curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
         $server_output = curl_exec($ch);
-		echo $server_output;
 		$httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 		$this->assertEquals($httpcode, 200);
         $json_out = json_decode($server_output);
