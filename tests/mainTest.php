c6e4bacf46b472be25ce94eafbb8f4ee495e2afc
zhaofeng-shu33 <616545598@qq.com>
2019-09-16 00:38:26 +0800

    use right get query parameter

--- tests/mainTest.php
+++ tests/mainTest.php
@@ -22,7 +22,7 @@ class mainTest extends TestCase
 	{
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $this->root . 'get_group_list.php?all=1');
-		curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true );
         $server_output = curl_exec($ch);
 		$httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 		$this->assertEquals($httpcode, 200);
@@ -34,8 +34,8 @@ class mainTest extends TestCase
     public function test_get_fixed_student()
     {
         $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $this->root . 'get_fixed_student.php?name=周一下午');
-		curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
+        curl_setopt($ch, CURLOPT_URL, $this->root . 'get_fixed_student.php?student_group=周一下午');
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         $server_output = curl_exec($ch);
 		$httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 		$this->assertEquals($httpcode, 200);
