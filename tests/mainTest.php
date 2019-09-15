0a265858c5be5f05a20ce5e95c0dfffd1ad86ec4
zhaofeng-shu33 <616545598@qq.com>
2019-09-16 00:47:23 +0800

    use urlencode

--- tests/mainTest.php
+++ tests/mainTest.php
@@ -34,7 +34,8 @@ class mainTest extends TestCase
     public function test_get_fixed_student()
     {
         $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $this->root . 'get_fixed_student.php?student_group=周一下午');
+        $group_name = '周一下午';
+        curl_setopt($ch, CURLOPT_URL, $this->root . 'get_fixed_student.php?student_group=' . urlencode($group_name));
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         $server_output = curl_exec($ch);
 		$httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
