986bef84f392d3b99995c3e7e400b4c5b2349fd0
zhaofeng-shu33 <616545598@qq.com>
2019-09-16 11:50:41 +0800

    local test passed for php72

--- db.sql
+++ db.sql
@@ -1,5 +1,6 @@
 -- mysql
 create database if not exists xingyu charset utf8mb4;
+set old_passwords=0;
 create user 'xingyu'@'localhost' identified by 'xingyu';
 grant all privileges on xingyu.* to 'xingyu'@'localhost';
 flush privileges;
