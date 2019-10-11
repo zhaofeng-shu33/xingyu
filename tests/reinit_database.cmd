83c6677d337b7c3c5dca38935a8e9a354cac3461
zhaofeng-shu33 <616545598@qq.com>
2019-10-11 21:31:25 +0800

    debug cmd

--- tests/reinit_database.cmd
+++ tests/reinit_database.cmd
@@ -2,4 +2,8 @@
 REM reinitialize the database
 mysql -uroot -p%MYSQL_ROOT_PASSWORD% < clear_db.sql
 mysql -uroot -p%MYSQL_ROOT_PASSWORD% < db.sql
-mysql -uxingyu -pxingyu -Dxingyu < tests\db_init.sql
+if [%1] == [] (
+    mysql -uxingyu -pxingyu -Dxingyu < tests\db_init.sql
+) else (
+    mysql -uxingyu -pxingyu -Dxingyu < %1
+)
