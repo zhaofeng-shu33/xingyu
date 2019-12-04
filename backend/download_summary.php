21387ee6523474acf0b2133320d0df21fd95821b
zhaofeng-shu33 <616545598@qq.com>
2019-12-04 23:34:07 +0800

    try to download sunshine

--- backend/download_summary.php
+++ backend/download_summary.php
@@ -16,7 +16,7 @@ foreach($organization_list as $key => $val){
 	array_push($valid_school_list, $key);
 }
 foreach($institution_list as $val){
-    array_push($valid_school_list, $val);
+	array_push($valid_school_list, $val);
 }
 
 if($school == null || array_search($school, $valid_school_list) === false){
@@ -28,7 +28,7 @@ header('Content-Disposition: attachment; filename="' . $date_str . $school .'.xl
 $spreadsheet = new Spreadsheet();
 $db = getDb();
 
-if($school == '童伴时光'){
+if(array_search($school, $institution_list) !== FALSE){
 	$spreadsheet->setActiveSheetIndex(0)
 		->setCellValue('A1', '志愿者姓名')
 		->setCellValue('B1', '所在学校')
@@ -36,7 +36,7 @@ if($school == '童伴时光'){
 		->setCellValue('D1', '活动时间')
 		->setCellValue('E1', '活动地点');
 	$school_map = $organization_list;
-	$sql = "SELECT s.name, s.school, a.name, a.time, a.location from ".getTablePrefix()."_activity as a, ".getTablePrefix()."_student as s, ".getTablePrefix()."_student_activity as sa where sa.student_id = s.id and sa.activity_id = a.id and a.institution = '童伴时光'";
+	$sql = "SELECT s.name, s.school, a.name, a.time, a.location from ".getTablePrefix()."_activity as a, ".getTablePrefix()."_student as s, ".getTablePrefix()."_student_activity as sa where sa.student_id = s.id and sa.activity_id = a.id and a.institution = '$school'";
 	$res=mysqli_query($db, $sql) or die(mysqli_error($db));
 	$rows = mysqli_fetch_all($res);
 	for($i=0; $i<count($rows); $i++){
@@ -47,7 +47,6 @@ if($school == '童伴时光'){
 		->setCellValue('D' . strval($i+2), $rows[$i][3])
 		->setCellValue('E' . strval($i+2), $rows[$i][4]);
 	}
-
 }
 else{
 	$spreadsheet->setActiveSheetIndex(0)
