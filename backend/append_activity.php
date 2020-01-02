0cb2872dd5652c1441b9a264053d51f4ac339d00
zhaofeng-shu33 <616545598@qq.com>
2020-01-02 21:27:07 +0800

    add space between bracket and keyword for some php files

--- backend/append_activity.php
+++ backend/append_activity.php
@@ -14,34 +14,33 @@ $semester_id = isset($jsondata->semester) ? $jsondata->semester : null;
 $openid = $jsondata->openid;
 $db = getDb();
 ensure_admin($db, $openid);
-if($semester_id == null){
+if ($semester_id == null) {
     $semester_id = get_current_semester($db);
-}
-elseif(gettype($semester_id) != 'integer'){
+} elseif (gettype($semester_id) != 'integer') {
     exitJson(6, 'invalid semester');    
 }
 
-if($week == null || gettype($week)!='integer'){
-    if($week == null)
+if ($week == null || gettype($week) != 'integer') {
+    if ($week == null)
         exitJson(1, 'null week');
     else
         exitJson(1, 'invalid week, type '.gettype($week));
 }
-if($name == null || $name == ''){
+if ($name == null || $name == '') {
     exitJson(2, 'null name');
 }
-if($list == null || gettype($list)!='array'){
+if ($list == null || gettype($list) != 'array') {
     exitJson(3, 'invalid student_list');
 }
 
 // set the location of the activity
 $location = get_location($name);
-if($location == ''){
+if ($location == '') {
     exitJson(10, 'group name does not contain location information');
 }
 
 $date_str = get_semester_start_date($db, $semester_id);
-if($date_str == null){
+if ($date_str == null) {
 	exitJson(8, 'not support semester_id provided');
 }
 $date = date_create($date_str);
@@ -52,23 +51,23 @@ $date_str = date_format($date, 'Y-m-d');
 $sql_check = 'select id from '.getTablePrefix()."_activity where name = '$name' and location = '$location' and time = '$date_str'";
 $res = mysqli_query($db, $sql_check) or die(mysqli_error($db));
 $row = mysqli_fetch_assoc($res);
-if($row['id'] == null){
+if ($row['id'] == null) {
     exitJson(5, 'activity not exists, with location = '.$location.' and time = '.$date_str);
 }
 $activity_id = $row['id'];
-foreach($list as $student){
+foreach($list as $student) {
     // first get student id
     $sql_s = 'select id from '.getTablePrefix()."_student where name = '$student'";
     $res_s = mysqli_query($db, $sql_s) or die(mysqli_error($db));
     $row_s = mysqli_fetch_assoc($res_s);
-    if($row_s['id'] == null){
+    if ($row_s['id'] == null) {
         exitJson(4, 'student '. $student. ' not exists in db');
     }    
     $student_id = $row_s['id'];
     $sql_s_a = 'select id from '.getTablePrefix()."_student_activity where student_id = $student_id and activity_id = $activity_id";
     $res_s_a = mysqli_query($db, $sql_s_a) or die(mysqli_error($db));
     $row_s_a = mysqli_fetch_assoc($res_s_a);
-    if($row_s_a['id'] != null){ // student has already joined this activity
+    if ($row_s_a['id'] != null) { // student has already joined this activity
         continue;
     }    
     $sql_r = 'insert into '.getTablePrefix()."_student_activity (student_id, activity_id) values ($student_id, $activity_id)";
